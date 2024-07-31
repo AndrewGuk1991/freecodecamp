cd426454d77d81b4e1ff6886a54a9279cf9af4f3
AndrewGuk1991 <gukandrey91@gmail.com>
2024-07-31 22:17:10 +0300

    commit-explore-differences-between-the-var-and-let-keywords

---
+++ explore-differences-between-the-var-and-let-keywords.js
@@ -0,0 +1,11 @@
+// Description:
+
+// Update the code so it only uses the let keyword.
+
+// var catName = "Oliver";
+// var catSound = "Meow!";
+
+// Solution:
+
+let catName = "Oliver";
+let catSound = "Meow!";

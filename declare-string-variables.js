b7ea1312f950156357b9c96df5c0d4e9f7b7f1ce
AndrewGuk1991 <gukandrey91@gmail.com>
2024-07-31 21:49:54 +0300

    commit-declare-string-variables

---
+++ declare-string-variables.js
@@ -0,0 +1,9 @@
+// Descrition:
+
+// Create two new string variables: myFirstName and myLastName 
+// and assign them the values of your first and last name, respectively.
+
+// Solution:
+
+var myFirstName = 'Andrew';
+var myLastName = 'Guk';

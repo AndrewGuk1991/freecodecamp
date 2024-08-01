8bc7bb4f0a0cc0659ede6aeb16fb5ba113396686
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-01 10:49:48 +0300

    commit-increment-a-number-with-javascript

---
+++ increment-a-number-with-javascript.js
@@ -0,0 +1,13 @@
+// Description:
+
+// Change the code to use the ++ operator on myVar.
+
+// let myVar = 87;
+
+// Only change code below this line
+
+// myVar = myVar + 1;
+
+Solution:
+
+myVar++;

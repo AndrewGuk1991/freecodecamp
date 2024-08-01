31fcc3cb8b44b3dedca2f0bfc6b003f559b5ca7c
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-01 11:44:21 +0300

    commit-finding-a-remainder-in-javascript

---
+++ finding-a-remainder-in-javascript.js
@@ -0,0 +1,11 @@
+// Description: 
+
+// Set remainder equal to the remainder of 11 divided by 3 using the remainder (%) operator.
+
+// const remainder = 0;
+
+Solution:
+
+let remainder = 2;
+
+remainder = 11 % 3

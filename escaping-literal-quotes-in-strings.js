3446aff1c61fdb2a606e836fa5d6ec2986eb30ee
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-03 12:33:42 +0300

    commit-escaping-literal-quotes-in-strings

---
+++ escaping-literal-quotes-in-strings.js
@@ -0,0 +1,10 @@
+// Description:
+
+// Use backslashes to assign a string to the myStr variable 
+// so that if you were to print it to the console, you would see:
+
+// I am a "double quoted" string inside "double quotes".
+
+// Solution:
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line

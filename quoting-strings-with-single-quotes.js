5df7a90b942273becf79c8eaba10ed12e97ecbdf
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-03 12:47:38 +0300

    commit-quoting-strings-with-single-quotes

---
+++ quoting-strings-with-single-quotes.js
@@ -0,0 +1,11 @@
+// Description:
+
+// Right now, the <a> tag in the string uses double quotes everywhere. 
+// You will need to change the outer quotes to single quotes so you can remove the escape characters.
+
+// const myStr = "<a href=\"http://www.example.com\" target=\"_blank\">Link</a>";
+
+// Solution:
+
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+console.log(myStr);

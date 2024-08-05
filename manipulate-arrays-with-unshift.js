55ea405bdd1da6156362c6a7bfd3500bd1309f56
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-05 23:36:32 +0300

    commit-manipulate-arrays-with-unshift

---
+++ manipulate-arrays-with-unshift.js
@@ -0,0 +1,12 @@
+// Description:
+
+// Add ["Paul", 35] to the beginning of the myArray variable using unshift().
+
+// Setup
+// const myArray = [["John", 23], ["dog", 3]];
+// myArray.shift();
+
+// Solution:
+
+// Only change code below this line
+myArray.unshift(["Paul", 35])

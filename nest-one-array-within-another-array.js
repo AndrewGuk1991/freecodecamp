7ef9ed46bba9808eff976ea4380456294161606f
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-05 23:08:44 +0300

    commit-nest-one-array-within-another-array

---
+++ nest-one-array-within-another-array.js
@@ -0,0 +1,11 @@
+// Description:
+
+// Create a nested array called myArray.
+
+// Only change code below this line
+// const myArray = [];
+
+// Solution:
+
+// Only change code below this line
+const myArray = [[]];

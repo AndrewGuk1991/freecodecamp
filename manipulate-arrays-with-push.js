055d579c503980535c4fb355cce449806fcff33f
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-05 23:23:49 +0300

    commit-manipulate-arrays-with-push

---
+++ manipulate-arrays-with-push.js
@@ -0,0 +1,12 @@
+// Description:
+
+// Push ["dog", 3] onto the end of the myArray variable.
+
+// Setup
+// const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+
+// Solution:
+
+myArray.push(["dog", 3])

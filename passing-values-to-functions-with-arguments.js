03fc192523bc3253ca07d2d800ed79dc4e6e6d2b
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-06 20:29:28 +0300

    commit-passing-values-to-functions-with-arguments

---
+++ passing-values-to-functions-with-arguments.js
@@ -0,0 +1,12 @@
+// Description:
+
+// Create a function called functionWithArgs that accepts two arguments and outputs their sum to the dev console.
+// Call the function with two numbers as arguments.
+
+// Solution:
+
+function functionWithArgs(param1, param2) {
+  console.log(param1 + param2);
+}
+
+functionWithArgs (1, 2)

928791ef2c1e703f3afb883618fd48414299cbb2
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-06 20:20:04 +0300

    commit-shopping-list

---
+++ shopping-list.js
@@ -0,0 +1,11 @@
+// Description:
+
+// Create a shopping list in the variable myList. The list should be a multi-dimensional array containing several sub-arrays.
+
+// The first element in each sub-array should contain a string with the name of the item. The second element should be a number representing the quantity i.e.
+
+// There should be at least 5 sub-arrays in the list.
+
+// Solution:
+
+const myList = [["Chocolate Bar", 15],["ice cream", 10],["candy", 9],["glass of coffee", 7],["milk", 5]];

89fc067bb172a71bb2664d6b0da91a0b336d72d4
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-06 20:24:53 +0300

    commit-write-reusable-javascript-with-functions

---
+++ write-reusable-javascript-with-functions.js
@@ -0,0 +1,12 @@
+// Description:
+
+// Create a function called reusableFunction which prints the string Hi World to the dev console.
+// Call the function.
+
+// Solution:
+
+function reusableFunction() {
+  console.log("Hi World");
+}
+
+reusableFunction()

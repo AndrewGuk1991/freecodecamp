6cad958bd9e6e4f783c487a2929e0fb876184932
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-06 20:33:12 +0300

    commit-return-a-value-from-a-function-with-return

---
+++ return-a-value-from-a-function-with-return.js
@@ -0,0 +1,11 @@
+// Description:
+
+// Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
+
+// Solution:
+
+function timesFive(num) {
+  return num * 5;
+}
+
+const answer = timesFive(5);

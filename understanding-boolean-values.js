6274c812aab327624d90030ce90c945dc226fc63
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-07 21:49:04 +0300

    commit-welcomeToBooleans

---
+++ understanding-boolean-values.js
@@ -0,0 +1,13 @@
+// Description:
+
+// Modify the welcomeToBooleans function so that it returns true instead of false.
+
+// Solution:
+
+function welcomeToBooleans() {
+  // Only change code below this line
+
+  return true; // Change this line
+
+  // Only change code above this line
+}

40b07c8ca1b319671d493ed33ab3469ceecf1b41
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-09 20:47:47 +0300

    commit-comparison-with-the-strict-equality-operator

---
+++ comparison-with-the-strict-equality-operator.js
@@ -0,0 +1,15 @@
+// Description:
+
+// Use the strict equality operator in the if statement so the function will return the string Equal when val is strictly equal to 7.
+
+// Solution:
+
+// Setup
+function testStrict(val) {
+  if (val === 7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testStrict(10);

43aa417ec26b6b73a3ac2b5ea6a29584fa37aa06
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-10 13:32:54 +0300

    commit-chaining-if-else-statements

---
+++ chaining-if-else-statements.js
@@ -0,0 +1,27 @@
+// Description:
+
+// Write chained if/else if statements to fulfill the following conditions:
+
+// num < 5 - return Tiny
+// num < 10 - return Small
+// num < 15 - return Medium
+// num < 20 - return Large
+// num >= 20 - return Huge
+
+// Solution:
+
+function testSize(num) {
+    // Only change code below this line
+  if (num < 5) {
+    return 'Tiny'
+  } else if (num < 10) {
+    return 'Small'
+  } else if (num < 15) {
+     return 'Medium'
+  } else if (num < 20) {
+    return 'Large'
+  } else if (num >= 20) {
+     return 'Huge'
+    // Only change code above this line
+  }
+}

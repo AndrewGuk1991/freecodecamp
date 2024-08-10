32ac2355ef673393f3718c4218d5431305cdffde
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-10 16:50:40 +0300

    commit-add-new-properties-to-a-javascript-object

---
+++ add-new-properties-to-a-javascript-object.js
@@ -0,0 +1,14 @@
+// Description:
+
+// Add a bark property to myDog and set it to a dog sound, such as "woof". You may use either dot or bracket notation.
+
+// Solution:
+
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.bark = 'woof'

6759a400d4a3262904cb018ab26dc5e33c5d2372
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-10 17:25:23 +0300

    commit-manipulating-complex-objects

---
+++ manipulating-complex-objects.js
@@ -0,0 +1,30 @@
+// Description:
+
+// Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings.
+
+// Solution:
+
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Jack Jones",
+    "title": "Guitar Man",
+    "release_year": 2000,
+    "formats": [
+      "CD",
+      "MP3",
+      "LP"
+    ],
+    "silver": true
+  }
+];

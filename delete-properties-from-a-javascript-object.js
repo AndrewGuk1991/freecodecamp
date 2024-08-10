0198c3390943d748b48d7ad88cd47ded3fa0cbea
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-10 16:52:57 +0300

    commit-delete-properties-from-a-javascript-object

---
+++ delete-properties-from-a-javascript-object.js
@@ -0,0 +1,18 @@
+// Description:
+
+// Delete the tails property from myDog. You may use either dot or bracket notation.
+
+// Solution:
+
+  // Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line
+
+delete myDog['tails']

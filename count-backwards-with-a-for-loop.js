a954f7a2d1a5b15ae1e61e56bd38de99d4f6cb3a
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-13 21:13:19 +0300

    commit-count-backwards-with-a-for-loop

---
+++ count-backwards-with-a-for-loop.js
@@ -0,0 +1,14 @@
+// Description:
+
+// Push the odd numbers from 9 through 1 to myArray using a for loop.
+
+// Solution:
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+
+for (let i = 9; i >= 1; i -= 2) {
+  myArray.push(i);
+}

eae80b3df272e0e079394b30f66d5ef6c096d6d9
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-13 21:46:33 +0300

    commit-generate-random-fractions-with-javascript

---
+++ generate-random-fractions-with-javascript.js
@@ -0,0 +1,14 @@
+// Description:
+
+// Change randomFraction to return a random number instead of returning 0.
+
+// Solution:
+
+function randomFraction() {
+
+  // Only change code below this line
+
+  return Math.random();
+
+  // Only change code above this line
+}

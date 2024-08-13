880e836fd816ed09eaa8cf6012274aaf44240809
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-13 21:59:02 +0300

    commit-use-the-parseint-function

---
+++ use-the-parseint-function.js
@@ -0,0 +1,11 @@
+// Description:
+
+// Use parseInt() in the convertToInteger function so it converts the input string str into an integer, and returns it.
+
+// Solution:
+
+function convertToInteger(str) {
+  return parseInt(str)
+}
+
+convertToInteger("56");

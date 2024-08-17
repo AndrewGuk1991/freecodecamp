3b5fc90b1de86c3697e6946bc30bc2494e1c720a
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-17 18:18:29 +0300

    commit-extract-matches

---
+++ extract-matches.js
@@ -0,0 +1,9 @@
+Description:
+
+Apply the .match() method to extract the string coding.
+
+Solution:
+
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/ ; // Change this line
+let result = extractStr.match(codingRegex); // Change this line

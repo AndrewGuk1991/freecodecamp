1e77ecaa9920dd5fed1f8ef4f47aacd8a802264b
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-17 21:13:03 +0300

    commit-match-ending-string-patterns

---
+++ match-ending-string-patterns.js
@@ -0,0 +1,9 @@
+// Description:
+
+// Use the anchor character ($) to match the string caboose at the end of the string caboose.
+
+// Solution:
+
+let caboose = "The last car on a train is the caboose";
+let lastRegex = /caboose$/; // Change this line
+let result = lastRegex.test(caboose);

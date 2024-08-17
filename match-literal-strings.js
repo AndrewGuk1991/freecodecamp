e35fc31a0a2b893c470cc8a9af3bef127662a9f2
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-17 17:47:23 +0300

    commit-match-literal-strings

---
+++ match-literal-strings.js
@@ -0,0 +1,9 @@
+Description:
+
+Complete the regex waldoRegex to find "Waldo" in the string waldoIsHiding with a literal match.
+
+Solution:
+
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let result = waldoRegex.test(waldoIsHiding);

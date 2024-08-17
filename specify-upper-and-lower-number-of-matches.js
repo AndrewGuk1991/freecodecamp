e73670c176e76e96fbe8b131abe740b647730f47
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-17 22:01:19 +0300

    commit-specify-upper-and-lower-number-of-matches

---
+++ specify-upper-and-lower-number-of-matches.js
@@ -0,0 +1,9 @@
+// Description:
+
+// Change the regex ohRegex to match the entire phrase Oh no only when it has 3 to 6 letter h's.
+
+// Solution:
+
+let ohStr = "Ohhh no";
+let ohRegex = /Oh{3,6} no/; // Change this line
+let result = ohRegex.test(ohStr);

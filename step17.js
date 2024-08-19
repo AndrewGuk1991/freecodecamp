110d7a2d933976fe2b664a2472e3f0941eb284dc
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-19 21:08:23 +0300

    commit-step17

---
+++ step17.js
@@ -0,0 +1,11 @@
+// Description:
+
+// You can also perform subtraction (-), multiplication (*), and division (/). Feel free to experiment with the operators and numbers in your console.log. When you are ready to move on, remove the console.log.
+
+// Solution:
+
+let count = 8;
+count + 1
+count - 1
+count * 1
+count / 1

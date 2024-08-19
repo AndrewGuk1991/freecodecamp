f69e73f872cf58cc8462f90a22d40774f5b89c98
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-19 20:33:36 +0300

    commit-step5

---
+++ step5.js
@@ -0,0 +1,8 @@
+// Description:
+
+// Print the value of the character variable to the console. Then, click the "Console" button to view the JavaScript console.
+
+// Solution:
+
+let character = 'Hello';
+console.log(character)

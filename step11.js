f435c97199d06b7b5c9644f202ba41812672f926
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-19 20:52:58 +0300

    commit-step11

---
+++ step11.js
@@ -0,0 +1,10 @@
+// Description:
+
+// To see this in action, change your secondCharacter assignment from "Test" to your character variable.
+
+// Then open the console to see what gets logged.
+
+// Solution:
+
+secondCharacter = "Test";
+secondCharacter = character;

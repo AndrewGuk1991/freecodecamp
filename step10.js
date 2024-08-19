835566c7eba915fc521b8060b761acc9dd2db079
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-19 20:48:20 +0300

    commit-step10

---
+++ step10.js
@@ -0,0 +1,12 @@
+// Description:
+
+// Assign the string "Test" to your secondCharacter variable below your declaration. Open the console to see how your log has changed.
+
+// Solution:
+
+let character = 'Hello';
+console.log(character);
+character = "World";
+let secondCharacter;
+secondCharacter = "Test";
+console.log(secondCharacter);

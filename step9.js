759eda8c974eaf8b5d351420df355c06bcd05e8f
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-19 20:45:20 +0300

    commit-step9

---
+++ step9.js
@@ -0,0 +1,11 @@
+// Description:
+
+// Add a console.log() to see what the value of your secondCharacter variable is.
+
+// Solution:
+
+let character = 'Hello';
+console.log(character);
+character = "World";
+let secondCharacter;
+console.log(secondCharacter)

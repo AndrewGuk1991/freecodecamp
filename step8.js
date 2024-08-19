298c4bfb72e181c13f02d55be9db2d082cac9b3f
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-19 20:43:23 +0300

    commit-step8

---
+++ step8.js
@@ -0,0 +1,11 @@
+// Description:
+
+// Use camel case to declare a new secondCharacter variable.
+
+// Solution:
+
+let character = 'Hello';
+console.log(character);
+character = "World";
+console.log(character);
+let secondCharacter;

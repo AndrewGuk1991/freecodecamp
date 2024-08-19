8dfe7e05e442d230cc2d5d7aab05fc5565dd1d47
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-19 21:03:34 +0300

    commit-step15

---
+++ step15.js
@@ -0,0 +1,8 @@
+// Description:
+
+// Use let to declare a count variable. Assign it the number 8. When using a number value, you do not use quotes.
+
+// Solution:
+
+let character = 'Hello';
+let count = 8;

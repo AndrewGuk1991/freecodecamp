1598c5bc8af97953d2dcbd38778210948371676e
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-20 18:09:41 +0300

    commit-step26

---
+++ step26.js
@@ -0,0 +1,9 @@
+// Description:
+
+// Use .push() to add the string "freeCodeCamp" to the end of your rows array. Add this code before your console.log so you can see the change you made to your array.
+
+// Solution:
+
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp")
+console.log(rows);

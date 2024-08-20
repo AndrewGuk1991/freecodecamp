e456e6b3ae37bb8c2745b99bda00a86fa2891ef1
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-20 17:58:17 +0300

    commit-step21

---
+++ step21.js
@@ -0,0 +1,8 @@
+// Description:
+
+// Update the third element of your rows array to be the number 10. Then print the rows array to your console.
+
+// Solution:
+
+rows[2] = 10;
+console.log(rows);

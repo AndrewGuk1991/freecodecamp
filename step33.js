eb5b428e752dc5ee2a8c44eaae53aa13831df11b
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-20 23:01:10 +0300

    commit-step33

---
+++ step33.js
@@ -0,0 +1,9 @@
+// Description:
+
+// In the upcoming steps, you'll explore each component of a loop in detail. For now, construct a for loop that includes the terms "iterator", "condition", and "iteration" for the three components. Keep the loop body, the section within the curly braces {}, empty.
+
+// Solution:
+
+for ("iterator"; "condition"; "iteration") {
+  
+}

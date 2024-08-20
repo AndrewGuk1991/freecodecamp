0ac8b4d988a30f3d1b8b4f62e10397c58a3616e2
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-20 23:04:15 +0300

    commit-step34

---
+++ step34.js
@@ -0,0 +1,9 @@
+// Description:
+
+// Replace the string "iterator" with a let declaration for the variable i. Assign it the value 0 to start. This will give the i variable the value 0 the first time your loop runs.
+
+// Solution:
+
+for (let i = 0; "condition"; "iteration") {
+
+}

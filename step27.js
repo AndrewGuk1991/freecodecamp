9b70b6c259cef424f11fe512802d87bad52ef004
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-20 18:11:49 +0300

    commit-step27

---
+++ step27.js
@@ -0,0 +1,11 @@
+// Description:
+
+// Create a new variable called popped and assign it the result of rows.pop(). Then, log popped to the console.
+
+// Solution:
+
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+let popped = rows.pop()
+console.log(popped);
+console.log(rows);

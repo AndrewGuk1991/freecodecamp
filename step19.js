72cf3cba3c7699ccde5d2b9947a090a30e7c0275
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-20 17:53:04 +0300

    commit-step19

---
+++ step19.js
@@ -0,0 +1,9 @@
+// Description:
+
+// Change your rows declaration to be an array with the strings "Naomi", "Quincy", and "CamperChan". The order of values in an array is important, so follow that order. Remember that strings are case-sensitive.
+
+// Solution:
+
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];

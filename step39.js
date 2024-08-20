9f6f6fa54e282d455550689799e10dfe1d38d2ba
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-20 23:17:06 +0300

    commit-step39

---
+++ step39.js
@@ -0,0 +1,9 @@
+// Description:
+
+// Unfortunately, now you cannot see what your loop is doing.
+
+// Use let to declare a result variable, and assign it an empty string. An empty string is represented by quotation marks with nothing between them, such as "".
+
+// Solution:
+
+let result = ''

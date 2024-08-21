2dbfde81e736a01397252e7900eb02b4795c9cbd
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-21 19:27:45 +0300

    commit-step42

---
+++ step42.js
@@ -0,0 +1,9 @@
+// Description:
+
+// In your for...of loop, use the addition operator to concatenate the row value to the result value.
+
+// Solution:
+
+for (const row of rows) {
+    result = result + row
+}

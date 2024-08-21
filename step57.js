c5c59ee26a09aa1c12aeb628e661e1e55c4f6b55
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-21 20:21:41 +0300

    commit-step57

---
+++ step57.js
@@ -0,0 +1,9 @@
+// Description:
+
+// This example would return "Professor Naomi". Update your padRow function to return the value of concatenating your character variable to the beginning of the name parameter.
+
+// Solution:
+
+function padRow(name) {
+    return character + name;
+}

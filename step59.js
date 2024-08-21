01f38b7930f4bd066c7831d6fde409d1e8579990
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-21 20:27:37 +0300

    commit-step59

---
+++ step59.js
@@ -0,0 +1,10 @@
+// Description:
+
+// To use your "Testing" value, return it out of the padRow function by updating your return statement to return only the test variable.
+
+// Solution:
+
+function padRow(name) {
+    const test = "Testing";  
+    return test;
+}

65ed72ca466cd5fc6652563c041fa4ea19de27f9
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-21 21:04:59 +0300

    commit-step69

---
+++ step69.js
@@ -0,0 +1,19 @@
+// Description:
+
+// Now it is time for a bit of math. Consider a three-row pyramid. If we want it centered, it would look something like:
+
+// Example Code
+// ··#··
+// ·###·
+// #####
+// Empty spaces have been replaced with interpuncts, or middle dots, for readability. If you extrapolate the pattern, you can see that the spaces at the beginning and end of a row follow a pattern.
+
+// Update your blank space strings to be repeated rowCount - rowNumber times.
+
+// Open up the console to see the result.
+
+// Solution:
+
+function padRow(rowNumber, rowCount) {
+    return " ".repeat(rowCount - rowNumber) + character.repeat(rowNumber) + " ".repeat(rowCount - rowNumber);
+}

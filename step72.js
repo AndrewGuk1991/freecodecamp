fcf8d3f5afde61fd9add8e3b121f4f3d06a6e1f5
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-21 21:15:23 +0300

    commit-step72

---
+++ step72.js
@@ -0,0 +1,12 @@
+// Description:
+
+// Because you are only increasing i by 1, you can use the increment operator ++. This operator increases the value of a variable by 1, updating the assignment for that variable. For example, test would become 8 here:
+
+// Example Code
+// let test = 7;
+// test++;
+// Replace your addition assignment with the increment operator for your loop iteration.
+
+// Solution:
+
+for (let i = 0; i < count; i++) {}

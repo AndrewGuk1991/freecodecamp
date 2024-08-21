20ca3393c97256d47b317b66b5c6c79731ff8c28
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-21 20:35:14 +0300

    commit-step62

---
+++ step62.js
@@ -0,0 +1,11 @@
+// Description:
+
+// Go ahead and remove the test declaration and return statement from your padRow function, so the function is empty again.
+
+// Solution:
+
+function padRow() {
+
+}
+const call = padRow();
+console.log(call);

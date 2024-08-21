921d89315186112238062dd215a4b9106d9648b3
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-21 21:25:25 +0300

    commit-step77

---
+++ step77.js
@@ -0,0 +1,13 @@
+// Description:
+
+// JavaScript also has support for multi-line comments. A multi-line comment starts with /* and ends with */.
+
+// Unlike a single-line comment, a multi-line comment will encapsulate multiple lines.
+
+// Use /* and */ to turn your current for loop, including the body, into a multi-line comment.
+
+// Solution:
+
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/

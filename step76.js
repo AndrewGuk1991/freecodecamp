20269c01157cd756109e66be8fb7b64e28bdb9a7
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-21 21:23:51 +0300

    commit-step76

---
+++ step76.js
@@ -0,0 +1,14 @@
+// Description:
+
+// Comments can be helpful for explaining why your code takes a certain approach, or leaving to-do notes for your future self.
+
+// In JavaScript, you can use // to leave a single-line comment in your code.
+
+// Add a single-line comment above your function to remind yourself to change the code to a different kind of loop.
+
+// Solution:
+
+// change the code to a different kind of loop
+for (let i = 1; i <= count; i++) {
+    rows.push(padRow(i, count));
+}

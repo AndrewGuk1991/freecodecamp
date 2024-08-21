10bbb307bedc9920fa2d8fc5c30d9d4096d86f47
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-21 19:53:36 +0300

    commit-step47

---
+++ step47.js
@@ -0,0 +1,7 @@
+// Description:
+
+// Declare a padRow function. Do not create any parameter variables yet. The function body should be empty. Remember that you need to use camel case for your naming convention.
+
+// Solution:
+
+function padRow () {}

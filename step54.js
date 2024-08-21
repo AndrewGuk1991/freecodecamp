5816640b7f39b22804666e2ec51cd30d287421cd
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-21 20:14:45 +0300

    commit-step54

---
+++ step54.js
@@ -0,0 +1,7 @@
+// Description:
+
+// Pass your own name as the argument for the name parameter in your padRow call. Remember that your name is a string, so you'll need to use quotes.
+
+// Solution:
+
+const call = padRow('Andrew');

b7ff8e10af60f0fd63e92bbd7d586bc4fbc7faf4
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-21 20:48:15 +0300

    commit-step65

---
+++ step65.js
@@ -0,0 +1,10 @@
+// Description:
+
+// Use the return keyword to return the value of the character variable, repeated rowNumber times.
+
+// Solution:
+
+function padRow(rowNumber, rowCount) {
+    return character.repeat(rowNumber)
+}
+

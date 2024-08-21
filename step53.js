14614b206fd0fc4a7c0610ddc6b46f8a6d3c5e3f
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-21 20:04:14 +0300

    commit-step53

---
+++ step53.js
@@ -0,0 +1,11 @@
+// Description:
+
+// A function does not have to return a hard-coded value. It can return the value stored in a variable. Parameters are special variables for a function, so they can also be returned.
+
+// Change your padRow function to return the name parameter directly.
+
+// Solution:
+
+function padRow(name) {
+    return name;
+}

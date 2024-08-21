12ee96f9690fb76bc2f1b2156b6a47c1ef905bc3
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-21 19:39:01 +0300

    commit-step45

---
+++ step45.js
@@ -0,0 +1,9 @@
+// Description:
+
+// Use the .repeat() method on your character, and give it i for the number.
+
+// Solution:
+
+for (let i = 0; i < count; i = i + 1) {
+    character.repeat(i)
+}

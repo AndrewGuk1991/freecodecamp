dd302636285df8ec21e1e5580e0c3c3fd54d6a3d
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-21 21:27:58 +0300

    commit-step78

---
+++ step78.js
@@ -0,0 +1,9 @@
+// Description:
+
+// Create an if statement with the boolean true as the condition. In the body, print the string "Condition is true".
+
+// Solution:
+
+if (true) {
+    console.log("Condition is true")
+}

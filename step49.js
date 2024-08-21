19c3f9c9ea8f8ac4ef7fcd74ac48b52651148363
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-21 19:57:28 +0300

    commit-step49

---
+++ step49.js
@@ -0,0 +1,7 @@
+// Description:
+
+// To see the result of calling your padRow function, declare a call variable and assign your existing padRow call to that variable.
+
+// Solution:
+
+const call = padRow();

e56620f6e4a9ef3d463e9de8a55d48b02b4f2700
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-21 21:31:13 +0300

    commit-step80

---
+++ step80.js
@@ -0,0 +1,11 @@
+// Description:
+
+// Now the string is no longer printing, because false is not true. But what about other values?
+
+// Try changing the condition to the string "false".
+
+// Solution:
+
+if ("false") {
+    console.log("Condition is true");
+}

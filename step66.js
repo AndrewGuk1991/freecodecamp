0f053d787dd4415605386d5b50f737fb8eb9d083
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-21 20:54:32 +0300

    commit-step66

---
+++ step66.js
@@ -0,0 +1,10 @@
+// Description:
+
+// Replace the character.repeat(i + 1) in your .push() call with a function call for your padRow function.
+
+// Solution:
+
+for (let i = 0; i < count; i = i + 1) {
+    rows.push(padRow())
+    
+}

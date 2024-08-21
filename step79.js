b82bee6a79a405628b020a805d3ed56637a03a60
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-21 21:29:18 +0300

    commit-step79

---
+++ step79.js
@@ -0,0 +1,11 @@
+// Description:
+
+// You'll see the string printed in the console, because true is in fact true.
+
+// Change the condition of your if statement to the boolean false.
+
+// Solution:
+
+if (false) {
+    console.log("Condition is true");
+}

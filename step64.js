5b36427f60f923396ec7db836e56bedc5f8190d5
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-21 20:42:21 +0300

    commit-step64

---
+++ step64.js
@@ -0,0 +1,9 @@
+// Description:
+
+// The best way to do this is by creating function parameters for them. Give your padRow function a rowNumber and rowCount parameter.
+
+// Solution:
+
+function padRow(rowNumber, rowCount) {
+
+}

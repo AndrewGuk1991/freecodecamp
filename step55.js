e741b1885595c65ecea3d7e44f0ee05791a68241
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-21 20:18:52 +0300

    commit-step55

---
+++ step55.js
@@ -0,0 +1,11 @@
+// Description:
+
+// Declare a sum variable and assign it the value of calling your addTwoNumbers function with 5 and 10 as the arguments. Log the sum variable to the console.
+
+// Solution:
+
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+const sum = addTwoNumbers(5, 10)
+console.log(sum)

c5ed85eebfa52e181a4414c8ec873c090f574113
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-22 18:34:56 +0300

    commit-step83

---
+++ step83.js
@@ -0,0 +1,13 @@
+// Description:
+
+// Add an else block to the else if block. Inside the else block, log the string "This is the else block" to the console.
+
+// Solution:
+
+if ("") {
+    console.log("Condition is true");
+} else if (5 < 10) {
+    console.log("5 is less than 10");
+} else {
+    console.log("This is the else block");
+}

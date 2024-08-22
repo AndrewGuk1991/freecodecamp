0400610c47c8667733cf77d1777e18a86ff9b977
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-22 19:22:38 +0300

    commit-step105

---
+++ step105.js
@@ -0,0 +1,9 @@
+// Description:
+
+// Use a multi-line comment to comment out this loop as well, to prepare for the next approach.
+
+// Solution:
+
+// for (let i = count; i > 0; i--) {
+//   rows.push(padRow(i, count));
+// }

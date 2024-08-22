16dc8147c441aa28caf8e42f1d84d6b9d2d5a158
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-22 18:32:43 +0300

    commit-step82

---
+++ step82.js
@@ -0,0 +1,13 @@
+// Description:
+
+// Below your if statement, add an else if statement that checks if 5 is less than 10. Then inside the body of the else if statement, log the string "5 is less than 10" to the console.
+
+// Check the console to see the results.
+
+// Solution:
+
+if ("") {
+    console.log("Condition is true");
+} else if (5 < 10) {
+    console.log("5 is less than 10")
+}

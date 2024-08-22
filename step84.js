da56d4eb3df0865f80084e7c913682d5e28001e1
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-22 18:37:19 +0300

    commit-step84

---
+++ step84.js
@@ -0,0 +1,10 @@
+// Description:
+
+// Now that you have practiced working with if...else if...else statements, you can remove them from your code.
+
+// Once you complete that, use let to declare a continueLoop variable and assign it the boolean false. Then use let to declare a done variable and assign it the value 0.
+
+// Solution:
+
+let continueLoop = false
+let done = 0

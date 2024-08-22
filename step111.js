33cce2c00adb7600ad9069fca2321f5d1d6f15ce
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-22 20:41:00 +0300

    commit-step111

---
+++ step111.js
@@ -0,0 +1,11 @@
+// Description:
+
+// Your pyramid is no longer inverted. This is because you are adding new rows to the end of the array.
+
+// Update your loop body to add new rows to the beginning of the array.
+
+// Solution:
+
+for (let i = 1; i <= count; i++) {
+    rows.unshift(padRow(i, count));
+}

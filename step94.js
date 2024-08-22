e008ec67e1dfb52e321eea5f2d459917f1af4dd0
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-22 18:59:15 +0300

    commit-step94

---
+++ step94.js
@@ -0,0 +1,16 @@
+// Description:
+
+// Your pyramid generator is still working. However, it could be possible to end up with an infinite loop again.
+
+// Because you are only checking if done is not equal to count, if done were to be larger than count your loop would go on forever.
+
+// Update your loop's condition to check if done is less than or equal to count.
+
+// Solution:
+
+let done = 0;
+
+while (done <= count) {
+  done++;
+  rows.push(padRow(done, count));
+}

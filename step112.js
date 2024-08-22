24926f13cd0971bf71df18038880a8997ba1f596
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-22 20:42:54 +0300

    commit-step112

---
+++ step112.js
@@ -0,0 +1,9 @@
+// Description:
+
+// What if you had a way to toggle between an inverted pyramid and a standard pyramid?
+
+// Start by declaring an inverted variable, and assigning it the value true. You are not changing this variable in your code, but you will need to use let so our tests can modify it later.
+
+// Solution:
+
+let inverted = true;

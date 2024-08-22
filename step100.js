16fc85046d01a5c6c86f03979cf1396fe7d68f31
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-22 19:13:40 +0300

    commit-step100

---
+++ step100.js
@@ -0,0 +1,11 @@
+// Description:
+
+// Because you are going to loop in the opposite direction, your loop needs to run while i is greater than 0. You can use the greater than operator > for this.
+
+// Set your loop's condition to run when i is greater than 0.
+
+// Solution:
+
+for (let i = count; i > 0; false) {
+
+}

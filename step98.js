a8f272329d9260c5e43bf79977a35327fabc5a0c
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-22 19:05:18 +0300

    commit-step98

---
+++ step98.js
@@ -0,0 +1,11 @@
+// Description:
+
+// That's a very clean and functional loop. Nice work! But there's still more to explore.
+
+// Use a multi-line comment to comment out your while loop.
+
+// Solution:
+
+// while (rows.length < count) {
+//   rows.push(padRow(rows.length + 1, count));
+// }

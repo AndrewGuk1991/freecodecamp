adfaf6075831f07ffc4daadd4dbdfcfb6b974f7b
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-22 20:44:53 +0300

    commit-step113

---
+++ step113.js
@@ -0,0 +1,11 @@
+// Description:
+
+// Use an if statement to check if inverted is true. Remember that you do not need to use an equality operator here.
+
+// Solution:
+
+for (let i = 1; i <= count; i++) {
+    if (inverted) {
+        rows.unshift(padRow(i, count));
+    }
+}

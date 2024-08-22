e72e935e7b720914ef6af94d22c30c80c35a5bd1
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-22 19:11:54 +0300

    commit-step99

---
+++ step99.js
@@ -0,0 +1,11 @@
+// Description:
+
+// What if you made your pyramid upside-down, or inverted? Time to try it out!
+
+// Start by creating a new for loop. Declare your iterator i and assign it the value of count, then use the boolean false for your condition and iteration statements.
+
+// Solution:
+
+for (let i = count; false; false) {
+
+}

f7f6080c435008599778a0cbea6be43e1f1b116c
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-22 18:44:34 +0300

    commit-step87

---
+++ step87.js
@@ -0,0 +1,14 @@
+// Description:
+
+// The equality operator == is used to check if two values are equal. To compare two values, you'd use a statement like value == 8.
+
+// Below done++ inside your loop, add an if statement. The statement should check if done is equal to count using the equality operator.
+
+// Solution:
+
+while (continueLoop) {
+    done++;
+    if (done == count) {
+
+    }
+}

c8f4733348abc905a8c9f6ca8afdc96b8de2b0ca
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-22 19:21:02 +0300

    commit-step104

---
+++ step104.js
@@ -0,0 +1,11 @@
+// Description:
+
+// Because you are only subtracting one from i, you can use the decrement operator --.
+
+// Replace your subtraction assignment with the decrement operator.
+
+// Solution:
+
+for (let i = count; i > 0; i--) {
+    rows.push(padRow(i, count));
+}

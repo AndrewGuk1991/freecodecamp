1193997c8afa801e9d9bbb641781507c1590d97e
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-22 18:52:01 +0300

    commit-step90

---
+++ step90.js
@@ -0,0 +1,7 @@
+// Description:
+
+// To make your pyramid generate again, push the result of calling padRow with done and count as the arguments to your rows array, similar to what you did in your first loop.
+
+// Solution:
+
+rows.push(padRow(done, count))

a860ee2b554279c072e1a7f039147b6d385a2499
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-22 20:54:18 +0300

    commit-step118

---
+++ step118.js
@@ -0,0 +1,12 @@
+// Description:
+
+// Nice work! Experiment with different values for your character, count, and inverted variables.
+
+// When you are ready to move on to your next project, set character to "!", count to 10, and inverted to false to continue.
+
+// Solution:
+
+const character = "!";
+const count = 10;
+const rows = [];
+let inverted = false;

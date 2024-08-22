3a81b562e4e4aeb7921a7c0dbc32fb4701487a08
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-22 19:17:38 +0300

    commit-step102

---
+++ step102.js
@@ -0,0 +1,11 @@
+// Description:
+
+// Again, push the result of calling padRow with your i and count variables to your rows array.
+
+// Open up the console to see the upside-down pyramid.
+
+// Solution:
+
+for (let i = count; i > 0; i = i - 1) {
+    rows.push(padRow(i, count))
+}

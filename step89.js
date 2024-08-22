3af8293c6eb8db4216c05904f6260f40df3d44b8
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-22 18:47:44 +0300

    commit-step89

---
+++ step89.js
@@ -0,0 +1,11 @@
+// Description:
+
+// When done has reached the value of count, we want the loop to stop executing.
+
+// Inside your if body, assign the boolean false to your continueLoop variable.
+
+// Solution:
+
+if (done === count) {
+    continueLoop = false
+}

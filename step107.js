61a7690306ea2a04ceba01029f2f53ab4081565e
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-22 19:27:54 +0300

    commit-step107

---
+++ step107.js
@@ -0,0 +1,10 @@
+// Description:
+
+// Use const to declare an unshifted variable, and assign it the result of calling .unshift() on your numbers array. Pass 5 as the argument. Then print your unshifted variable.
+
+// Solution:
+
+const numbers = [1, 2, 3];
+const unshifted = numbers.unshift(5)
+console.log(unshifted);
+console.log(numbers);

837de131f2941dae8cc78705a267c8c9a437c2a9
AndrewGuk1991 <gukandrey91@gmail.com>
2024-08-22 20:47:43 +0300

    commit-step115

---
+++ step115.js
@@ -0,0 +1,14 @@
+// Description:
+
+// Add an else block to your if block.
+
+// Solution:
+
+for (let i = 1; i <= count; i++) {
+    if (inverted) {
+        rows.unshift(padRow(i, count));
+    } else {
+
+    }
+
+}

5eda1fe972e5542f5405e482448603e6623be9e0
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-01 18:34:39 +0300

    commit-step-18-project-3

---
+++ step-18-project-3.js
@@ -0,0 +1,9 @@
+// Description: 
+
+// Use querySelector() to get the other two button elements using their ids: button2 and button3. Store them in variables called button2 and button3. Remember to use const.
+
+// Solution:
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");

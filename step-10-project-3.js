525af491ba7dc627ea7c3549e4bee6e6a46ec577
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-01 17:29:30 +0300

    commit-step-10-project-3

---
+++ step-10-project-3.js
@@ -0,0 +1,15 @@
+// Description: 
+
+// Declare two more variables named monsterHealth and inventory.
+
+// For your inventory variable, assign it the value of an array containing the string "stick".
+
+// Solution: 
+
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeaponIndex = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];

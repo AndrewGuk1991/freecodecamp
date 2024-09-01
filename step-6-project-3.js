08342caaf9fd3650d01cbb830f935b3495b3f0bc
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-01 17:18:44 +0300

    commit-step-6-project-3

---
+++ step-6-project-3.js
@@ -0,0 +1,9 @@
+// Description: 
+
+// Remove your console.log("Hello World"); line to begin writing your project code.
+
+// Use the let keyword to declare a variable called xp and assign it the value of 0, a number.
+
+// Solution: 
+
+let xp = 0;

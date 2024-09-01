11bcac08a1c172bb60b8ebd0b305e82d0eded256
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-01 17:23:12 +0300

    commit-step-8-project-3

---
+++ step-8-project-3.js
@@ -0,0 +1,10 @@
+// Description: 
+
+// Create another variable called currentWeaponIndex and set it to 0
+
+// Solution: 
+
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeaponIndex = 0;

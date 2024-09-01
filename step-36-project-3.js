5937f99173450a160e2d1e2aecfd797b48948fd2
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-01 21:04:46 +0300

    commit-step-36-project-3

---
+++ step-36-project-3.js
@@ -0,0 +1,8 @@
+// Description:
+
+// Use dot notation to set the onclick property of your button1 to the function reference of goStore. Note that button1 is already declared, so you don't need to use let or const.
+
+// Solution: 
+
+// initialize buttons
+button1.onclick = goStore;

7c3fefb33a5c83ff9cff6826060b04fe3aeee5e4
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-01 21:17:33 +0300

    commit-step-39-project-3

---
+++ step-39-project-3.js
@@ -0,0 +1,11 @@
+// Description: 
+
+// Now, add a line that updates the text of button2 to say "Buy weapon (30 gold)" and update the text of button3 to say "Go to town square".
+
+// Solution: 
+
+function goStore() {
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+}

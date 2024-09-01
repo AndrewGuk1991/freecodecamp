3cd9a8154dd662fa9aff3a850557886f0872ac43
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-01 17:20:48 +0300

    commit-step-7-project-3

---
+++ step-7-project-3.js
@@ -0,0 +1,9 @@
+// Description: 
+
+// Initialize another variable called health with a value of 100, and a variable called gold with a value of 50.
+
+// Solution: 
+
+let xp = 0;
+let health = 100;
+let gold = 50;

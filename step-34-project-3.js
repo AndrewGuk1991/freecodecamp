95a15b483f093652e76629d109c45fd84d8be165
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-01 20:32:11 +0300

    commit-step-34-project-3

---
+++ step-34-project-3.js
@@ -0,0 +1,9 @@
+// Description: 
+
+// Now create a fightDragon function that prints "Fighting dragon." to the console.
+
+// Solution: 
+
+function fightDragon() {
+    console.log("Fighting dragon.");
+}

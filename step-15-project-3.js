08fbb22cde5b958fb62fafad71ff39323637b1ea
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-01 18:27:32 +0300

    commit-step-15-project-3

---
+++ step-15-project-3.js
@@ -0,0 +1,7 @@
+// Description: 
+
+// Create a button1 variable and use querySelector() to assign it your element with the id of button1. Remember that CSS id selectors are prefixed with a #.
+
+// Solution:
+
+let button1 = document.querySelector("#button1")

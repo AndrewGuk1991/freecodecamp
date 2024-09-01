0aa7e664466c63eee7d5011a799dd54afa389399
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-01 18:32:31 +0300

    commit-step-17-project-3

---
+++ step-17-project-3.js
@@ -0,0 +1,9 @@
+// Description:
+
+// button1 is a variable that is not going to be reassigned. If you are not going to assign a new value to a variable, it is best practice to use the const keyword to declare it instead of the let keyword. This will tell JavaScript to throw an error if you accidentally reassign it.
+
+// Change your button1 variable to be declared with the const keyword.
+
+// Solution:
+
+const button1 = document.querySelector("#button1");

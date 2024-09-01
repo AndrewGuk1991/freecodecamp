7cf622a9206cbbc8bd2a13c7aed9ba1d1293c3ed
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-01 21:07:18 +0300

    commit-step-37-project-3

---
+++ step-37-project-3.js
@@ -0,0 +1,12 @@
+// Description: 
+
+// Using the same syntax, set the onclick properties of button2 and button3 to goCave and fightDragon respectively.
+
+// Once you have done that, open your console and try clicking the buttons on your project.
+
+// Solution: 
+
+// initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;

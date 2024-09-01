fb2e3dd226472c6f51a79dc35db7f72c6860de00
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-01 21:15:05 +0300

    commit-step-38-project-3

---
+++ step-38-project-3.js
@@ -0,0 +1,9 @@
+// Description:
+
+// When a player clicks your Go to store button, you want to change the buttons and text. Remove the code inside the goStore function and add a line that updates the text of button1 to say "Buy 10 health (10 gold)".
+
+// Solution: 
+
+function goStore() {
+    button1.innerText = "Buy 10 health (10 gold)";
+}

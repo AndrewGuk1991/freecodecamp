652abfd16d26392f80a6e0593699b5182cd7896c
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-01 20:28:06 +0300

    commit-step-32-project-3

---
+++ step-32-project-3.js
@@ -0,0 +1,9 @@
+// Description: 
+
+// For now, make your goStore function output the message "Going to store." to the console.
+
+// Solution: 
+
+function goStore() {
+    console.log("Going to store.");
+}

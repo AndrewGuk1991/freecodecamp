c6f6345ff570f6aa0d54355bafa91e7c2b4544c2
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-02 23:24:16 +0300

    commit-step-50-project-3

---
+++ step-50-project-3.js
@@ -0,0 +1,12 @@
+// Description: 
+
+// Add a new property with a key of "Number of legs" and value of 4 to the cat object.
+
+// Open up the console to see the output.
+
+// Solution:
+
+const cat = {
+    name: "Whiskers",
+    "Number of legs": 4
+};

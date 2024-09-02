7f569923e581c29c6a922af7cc6a36442bcc4790
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-02 23:19:53 +0300

    commit-step-48-project-3

---
+++ step-48-project-3.js
@@ -0,0 +1,10 @@
+// Description: 
+
+// For now, create a const variable called cat and assign it an empty object {}.
+
+// Below that cat variable, add a console.log(cat) statement to see the object in the console.
+
+// Solution: 
+
+const cat = {}
+console.log(cat)

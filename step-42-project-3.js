9b1adc331cad85ce637cca2235fa6ff8b4b3ea09
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-02 22:56:58 +0300

    commit-step-42-project-3

---
+++ step-42-project-3.js
@@ -0,0 +1,9 @@
+// Description: 
+
+// Create three new empty functions called buyHealth, buyWeapon, and goTown.
+
+// Solution: 
+
+function buyHealth() { }
+function buyWeapon() { }
+function goTown() { }

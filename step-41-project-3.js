071745bcf8566188a30918139157bced311c886f
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-02 22:54:07 +0300

    commit-step-41-project-3

---
+++ step-41-project-3.js
@@ -0,0 +1,15 @@
+// Description: 
+
+// Now you need to modify your display text. Change the innerText property of the text variable to be "You enter the store.".
+
+// Solution: 
+
+function goStore() {
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+    text.innerText = "You enter the store.";
+}

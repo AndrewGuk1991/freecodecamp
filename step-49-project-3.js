88e825a15963416de7ab82463d0f68c22154ff53
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-02 23:21:58 +0300

    commit-step-49-project-3

---
+++ step-49-project-3.js
@@ -0,0 +1,11 @@
+// Description: 
+
+// Inside your cat object, add a new property. The key should be name and the value should be the string "Whiskers".
+
+// Open up the console to see the updates to your object.
+
+// Solution: 
+
+const cat = {
+    name: "Whiskers"
+}

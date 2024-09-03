d836942b2235f1dbfd88efad7706b375513b293e
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-03 21:32:34 +0300

    commit-step-56-project-3

---
+++ step-56-project-3.js
@@ -0,0 +1,12 @@
+// Description: 
+
+// Give your empty button text array three string elements. Use the three strings being assigned to the button innerText properties in the goTown function. Remember that array values are separated by commas.
+
+// Solution: 
+
+const locations = [
+    {
+        name: "town square",
+        "button text": ["Go to store", "Go to cave", "Fight dragon"]
+    }
+];

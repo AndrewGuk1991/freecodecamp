c26c348c8b9ffacfebce0e11e60eae64cfa47c40
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-03 21:26:42 +0300

    commit-step-54-project-3

---
+++ step-54-project-3.js
@@ -0,0 +1,11 @@
+// Description: 
+
+// Inside your locations array, add an object. Inside that object add a key called name with a value of "town square".
+
+// Solution: 
+
+const locations = [
+    {
+        name: "town square"
+    }
+];

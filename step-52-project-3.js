87f60fa86d62f48a44a767eb8f5eab75050166b4
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-03 21:23:27 +0300

    commit-step-52-project-3

---
+++ step-52-project-3.js
@@ -0,0 +1,9 @@
+// Description: 
+
+// Update your console statement to use bracket notation to access the property "Number of legs" of the cat object.
+
+// Open up the console to see the output.
+
+// Solution: 
+
+console.log(cat["Number of legs"]);

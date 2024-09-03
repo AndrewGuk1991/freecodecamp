5202a0d40f93a4e066f371749617a3bdae088da4
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-03 22:26:12 +0300

    commit-step-71-project-3

---
+++ step-71-project-3.js
@@ -0,0 +1,14 @@
+// Description: 
+
+// Add a third object to the locations array. Give it the same properties as the other two objects.
+
+// Set name to cave. Set button text to an array with the strings "Fight slime", "Fight fanged beast", and "Go to town square". Set the "button functions" to an array with the variables fightSlime, fightBeast, and goTown. Set the text property to "You enter the cave. You see some monsters.".
+
+// Solution: 
+
+{
+    name: "cave",
+    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+    "button functions": [fightSlime, fightBeast, goTown],
+     text: "You enter the cave. You see some monsters."
+}

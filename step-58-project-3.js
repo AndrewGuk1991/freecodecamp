d4aff5e56f3dc9a753fd29a7587966525855a40c
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-03 21:44:23 +0300

    commit-step-58-project-3

---
+++ step-58-project-3.js
@@ -0,0 +1,14 @@
+// Description: 
+
+// Add one final property to the object named text. Give this property the same string value as the one assigned to text.innerText in the goTown function.
+
+// Solution: 
+
+const locations = [
+    {
+        name: "town square",
+        "button text": ["Go to store", "Go to cave", "Fight dragon"],
+        "button functions": [goStore, goCave, fightDragon],
+        text: "You are in the town square. You see a sign that says \"Store\"."
+    }
+];

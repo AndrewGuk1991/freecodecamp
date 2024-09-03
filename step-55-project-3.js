cd963e811f1e810fb9d6aa6c1962074732096e97
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-03 21:29:10 +0300

    commit-step-55-project-3

---
+++ step-55-project-3.js
@@ -0,0 +1,14 @@
+// Description: 
+
+// Just like array values, object properties are separated by a comma. Add a comma after your name property and add a button text property with the value of an empty array.
+
+// Since the property name has a space in it, you will need to surround it with quotes.
+
+// Solution: 
+
+const locations = [
+    {
+        name: "town square",
+        "button text": []
+    }
+];

5cbd1576dc85e1102ac5fd249bce915ae65b2e1f
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-03 22:04:22 +0300

    commit-step-66-project-3

---
+++ step-66-project-3.js
@@ -0,0 +1,15 @@
+// Description:
+
+// Now update button2.innerText and button3.innerText to be assigned the second and third values of the "button text" array, respectively.
+
+// Solution: 
+
+function update(location) {
+    button1.innerText = location["button text"][0];
+    button2.innerText = location["button text"][1];
+    button3.innerText = location["button text"][2];
+    button1.onclick = goStore;
+    button2.onclick = goCave;
+    button3.onclick = fightDragon;
+    text.innerText = "You are in the town square. You see a sign that says \"Store\".";
+}

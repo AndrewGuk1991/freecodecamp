15e6b42e2bafbca19489e5fa4b653668acce0056
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-03 21:56:34 +0300

    commit-step-62-project-3

---
+++ step-62-project-3.js
@@ -0,0 +1,9 @@
+// Description: 
+
+// Now it is time to use your update function. Pass in your locations array into the update function call.
+
+// Solution: 
+
+function goTown() {
+    update(locations);
+}

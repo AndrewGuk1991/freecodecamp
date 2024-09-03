7f5c458156225fe234d8f5da1fdab3a90cd2116c
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-03 21:18:35 +0300

    commit-step-53-project-3

---
+++ step-51-project-3.js
@@ -0,0 +1,9 @@
+// Description: 
+
+// Update your console statement to access the name property of the cat object using dot notation.
+
+// Open up the console to see the name of "Whiskers" logged to the console.
+
+// Solution: 
+
+console.log(cat.name)

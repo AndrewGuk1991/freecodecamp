830c975eecfbd1615c9fc41af66fbeb31ca6e1df
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-03 22:17:03 +0300

    commit-step-68-project-3

---
+++ step-68-project-3.js
@@ -0,0 +1,15 @@
+// Description: 
+
+// Finally, update the text.innerText assignment to equal the text from the location object. However, instead of using bracket notation, use dot notation.
+
+// Solution: 
+
+function update(location) {
+    button1.innerText = location["button text"][0];
+    button2.innerText = location["button text"][1];
+    button3.innerText = location["button text"][2];
+    button1.onclick = location["button functions"][0];
+    button2.onclick = location["button functions"][1];
+    button3.onclick = location["button functions"][2];
+    text.innerText = location.text;
+}

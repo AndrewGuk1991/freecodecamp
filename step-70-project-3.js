b11d0fdb70103c99856cfd705bcfdc8e78e26a92
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-03 22:22:14 +0300

    commit-step-70-project-3

---
+++ step-70-project-3.js
@@ -0,0 +1,8 @@
+// Description: 
+
+// Create two more empty functions named fightSlime and fightBeast. These functions will be used in your upcoming cave object.
+
+// Solution: 
+
+function fightSlime () {}
+function fightBeast () {}

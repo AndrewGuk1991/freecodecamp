992f1cd3b4c98cd3aac2073dc24ba85b01ab6e45
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-04 18:42:28 +0300

    commit-step-82-project

---
+++ step-82-project-3.js
@@ -0,0 +1,11 @@
+// Description: 
+
+// Inside your buyWeapon function, add an if statement to check if gold is greater than or equal to 30.
+
+// Solution:
+
+function buyWeapon() {
+    if (gold >= 30) {
+
+    }
+}

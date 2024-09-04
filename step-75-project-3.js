5b62c4c55a8d0f60bc08d00bade7587aa7849d24
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-04 18:21:54 +0300

    commit-step-75-project

---
+++ step-75-project-3.js
@@ -0,0 +1,12 @@
+// Description:
+
+// There is a shorthand way to add or subtract from a variable called compound assignment. For example, changing num = num + 5 to compound assignment would look like num += 5.
+
+// Update both lines inside your buyHealth function to use compound assignment.
+
+// Solution: 
+
+function buyHealth() {
+    gold -= 10;
+    health += 10;
+}

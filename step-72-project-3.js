7ca462d7c40f7e0f69472a8309c9d9b6df193720
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-04 18:15:18 +0300

    commit-step-72-project-3

---
+++ step-72-project-3.js
@@ -0,0 +1,11 @@
+// Description: 
+
+// Now that you have a "cave" location object, update your goCave function to call update and pass that new "cave" location. Remember that this is the third element in your locations array.
+
+// Don't forget to remove your console.log call!
+
+// Solution: 
+
+function goCave() {
+    update(locations[2])
+}

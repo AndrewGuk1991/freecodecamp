843555e6a7207be3d1e5f5fc1a5f4f3ef704e922
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-04 20:34:14 +0300

    commit-step-97-project-3

---
+++ step-97-project-3.js
@@ -0,0 +1,13 @@
+// Description: 
+
+// Now it is time to test your buyWeapon function. Right now, the gold amount is set to 50. But to properly see the results of your buyWeapon function, the amount should be set to something higher.
+
+// Update the gold amount to 250.
+
+// NOTE: The HTML has already been updated to reflect this change.
+
+// To test your buyWeapon function, open up the console. Then click on the "Go to store" button followed by the "Buy weapon (30 gold)" button four times.
+
+// Solution: 
+
+let gold = 250;

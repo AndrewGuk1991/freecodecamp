af2e687aabddf33c2f7d3d5097df0491af3c978f
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-04 18:45:43 +0300

    commit-step-83-project-3

---
+++ step-83-project-3.js
@@ -0,0 +1,11 @@
+// Description: 
+
+// Similar to your buyHealth function, set gold equal to 30 less than its current value. Make sure this is inside your if statement.
+
+// Solution: 
+
+function buyWeapon() {
+    if (gold >= 30) {
+        gold -= 30
+    }
+}

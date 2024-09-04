e97edd94034a5ec4fa265d5336942f59936ca349
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-04 18:18:01 +0300

    commit-step-73-project-3

---
+++ step-73-project-3.js
@@ -0,0 +1,9 @@
+// Description: 
+
+// Now that your "store" and "cave" locations are complete, you can code the actions the player takes at those locations. Inside the buyHealth function, set gold equal to gold minus 10.
+
+// Solution: 
+
+function buyHealth() {
+    gold = gold - 10
+}

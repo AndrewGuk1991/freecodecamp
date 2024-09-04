96bf5ce6396a1ad4f35e0d30755e9e68da7de846
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-04 18:20:10 +0300

    commit-step-74-project-3

---
+++ step-74-project-3.js
@@ -0,0 +1,10 @@
+// Description: 
+
+// After the gold is updated, add a line to set health equal to health plus 10.
+
+// Solution: 
+
+function buyHealth() {
+    gold = gold - 10;
+    health = health + 10;
+}

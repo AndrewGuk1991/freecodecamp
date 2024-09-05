a6e7a834c2a5fcbe2019db7d2fd8bf621abe0a58
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-05 19:00:00 +0300

    commit-step-103-project-3

---
+++ step-103-project-3.js
@@ -0,0 +1,11 @@
+// Description: 
+
+// Players should not be able to sell their only weapon. Inside the sellWeapon function, add an if statement with a condition that checks if the length of the inventory array is greater than 1.
+
+// Solution: 
+
+function sellWeapon() {
+    if (inventory.length > 1) {
+
+    }
+}

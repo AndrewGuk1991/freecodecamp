f9be9cb34d71b477b34ee49fec2093b517b97b18
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-05 19:04:57 +0300

    commit-step-105-project-3

---
+++ step-105-project-3.js
@@ -0,0 +1,15 @@
+// Description: 
+
+// The next step is to create a variable called currentWeapon
+
+// Use the let keyword to create a variable named currentWeapon. Don't assign it a value yet.
+
+// Solution: 
+
+function sellWeapon() {
+    if (inventory.length > 1) {
+        gold += 15;
+        goldText.innerText = gold;
+        let currentWeapon;
+    }
+}

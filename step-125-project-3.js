d2e05a59cec08df6622850bccf742b52bcd389a0
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-06 20:01:23 +0300

    commit-step-125-project-3

---
+++ step-125-project-3.js
@@ -0,0 +1,14 @@
+// Description: 
+
+// Update healthText.innerText and monsterHealthText.innerText to equal health and monsterHealth.
+
+// Solution: 
+
+function attack() {
+    text.innerText = "The " + monsters[fighting].name + " attacks.";
+    text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
+    health -= monsters[fighting].level;
+    monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+    healthText.innerText = health;
+    monsterHealthText.innerText = monsterHealth;
+}

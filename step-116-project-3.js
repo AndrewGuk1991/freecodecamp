b13a608a924fbd1af2e0dd92af0f0d268afed8b6
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-06 19:16:59 +0300

    commit-step-116-project-3

---
+++ step-116-project-3.js
@@ -0,0 +1,9 @@
+// Description: 
+
+// In the goFight function, call your update function with the fourth object in locations as an argument.
+
+// Solution: 
+
+function goFight() {
+    update(locations[3])
+}

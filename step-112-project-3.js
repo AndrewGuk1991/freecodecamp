fc8dbb021799d00ef3af2dbc3073b50071fc8c99
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-06 19:05:42 +0300

    commit-step-112-project-3

---
+++ step-112-project-3.js
@@ -0,0 +1,12 @@
+// Description: 
+
+// In your fightSlime function, set fighting equal to 0 - the index of slime in the monsters array. Remember that you already declared fighting earlier in your code, so you do not need let or const here.
+
+// On the next line, call the goFight function.
+
+// Solution: 
+
+function fightSlime() {
+    fighting = 0;
+    goFight()
+}

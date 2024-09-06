d449ebe626f055fbc9895e9c30245e3d5c9ff99f
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-06 19:08:20 +0300

    commit-step-113-project-3

---
+++ step-113-project-3.js
@@ -0,0 +1,15 @@
+// Description: 
+
+// Following the same pattern as the fightSlime function, use that code in the fightBeast and fightDragon functions. Remember that beast is at index 1 and dragon is at index 2. Also, remove the console.log call from your fightDragon function.
+
+// Solution: 
+
+function fightBeast() {
+    fighting = 1;
+    goFight();
+}
+
+function fightDragon() {
+    fighting = 2;
+    goFight();
+}

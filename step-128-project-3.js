706f8ff809ddee21505e8aa400990d193732bdac
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-07 09:51:40 +0300

    commit-step-128-project-3

---
+++ step-128-project-3.js
@@ -0,0 +1,8 @@
+// Description: 
+
+// At the end of your code, create the defeatMonster and lose functions. Leave them empty for now.
+
+// Solution: 
+
+function defeatMonster () {}
+function lose () {}

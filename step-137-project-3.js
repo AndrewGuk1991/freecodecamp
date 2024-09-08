ff6c2f2dd89f93b233ec41abe0623afdde06ccbc
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-08 10:06:14 +0300

    commit-step-137-project-3

---
+++ step-137-project-3.js
@@ -0,0 +1,9 @@
+// Description: 
+
+// In the lose function, call the update function and pass in the sixth object of your locations array. Note that you haven't created this object just yet.
+
+// Solution: 
+
+function lose() {
+    update(locations[5])
+}

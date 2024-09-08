74c59091545cdc241d4e44a65c3aa68b8754473c
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-08 12:23:51 +0300

    commit-step-142-project-3

---
+++ step-142-project-3.js
@@ -0,0 +1,9 @@
+// Description: 
+
+// After the lose function, create a function called winGame. Inside the winGame function, call the update function and pass in locations[6].
+
+// Solution: 
+
+function winGame() {
+    update(locations[6]);
+}

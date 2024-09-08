42b84feab1da36006cc56db78a14e20d611c51e6
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-08 12:28:01 +0300

    commit-step-143-project-3

---
+++ step-143-project-3.js
@@ -0,0 +1,18 @@
+// Description: 
+
+// Add another object in the locations array. Everything should be the same as the lose object, except the name should be "win" and the text should be "You defeat the dragon! YOU WIN THE GAME! &#x1F389;".
+
+// Solution:
+
+{
+    name: "lose",
+        "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+            "button functions": [restart, restart, restart],
+                text: "You die. &#x2620;"
+},
+{
+    name: "win",
+        "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+            "button functions": [restart, restart, restart],
+                text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;"
+}

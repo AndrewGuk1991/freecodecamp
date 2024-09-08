ee3f9f6c2912a3c4ae9dd2369ca9386b8e2331ac
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-08 12:18:22 +0300

    commit-step-140-project-3

---
+++ step-140-project-3.js
@@ -0,0 +1,25 @@
+// Description: 
+
+// Back to your attack function - inside the else if block, create another if and else statement. If the player is fighting the dragon (fighting would be 2), call the winGame function. Move the defeatMonster() call to the else block.
+
+// For this step, you will need to use the strict equality (===) operator to check if fighting is equal to 2.
+
+// Solution: 
+
+function attack() {
+    text.innerText = "The " + monsters[fighting].name + " attacks.";
+    text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
+    health -= monsters[fighting].level;
+    monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+    healthText.innerText = health;
+    monsterHealthText.innerText = monsterHealth;
+    if (health <= 0) {
+        lose();
+    } else if (monsterHealth <= 0) {
+        if (fighting === 2) {
+            winGame();
+        } else {
+            defeatMonster();
+        }
+    }
+}

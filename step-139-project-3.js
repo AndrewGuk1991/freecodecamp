87dd2222d7147b505dbbee603bfa268ecd31c4b0
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-08 12:13:30 +0300

    commit-step-139-project-3

---
+++ step-139-project-3.js
@@ -0,0 +1,14 @@
+// Description: 
+
+// In the locations array, add another object at the end. Set the name property to "lose", set "button text" to an array with three "REPLAY?" strings, set "button functions" to an array with three restart variables, and set text to "You die. &#x2620;".
+
+// In a later step, you will update the code for the &#x2620; emoticon text to properly display on the page.
+
+// Solution: 
+
+{
+    name: "lose",
+        "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+            "button functions": [restart, restart, restart],
+                text: "You die. &#x2620;"
+}

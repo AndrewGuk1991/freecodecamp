9f71f431706b12e9eca65b152732f12b5146f6ae
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-08 12:21:28 +0300

    commit-step-141-project-3

---
+++ step-141-project-3.js
@@ -0,0 +1,14 @@
+// Description: 
+
+// In the update function, change text.innerText to text.innerHTML.
+
+// Solution: 
+
+monsterStats.style.display = "none";
+button1.innerHTML = location["button text"][0];
+button2.innerHTML = location["button text"][1];
+button3.innerHTML = location["button text"][2];
+button1.onclick = location["button functions"][0];
+button2.onclick = location["button functions"][1];
+button3.onclick = location["button functions"][2];
+text.innerHTML = location.text;

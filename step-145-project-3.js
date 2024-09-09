bdfa07d5abfe8c457a616f3d2a0d69a779948980
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-09 18:12:01 +0300

    commit-step-145-project-3

---
+++ step-145-project-3.js
@@ -0,0 +1,9 @@
+// Description: 
+
+// Below your attack function, create an empty function named getMonsterAttackValue. It should take level as a parameter.
+
+// Solution: 
+
+function getMonsterAttackValue (level) {
+
+}

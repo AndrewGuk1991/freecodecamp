05c84986e09eb112c7c14585c2324a09b6c90879
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-10 19:46:44 +0300

    commit-step-161-project-3

---
+++ step-161-project-3.js
@@ -0,0 +1,12 @@
+// Description:
+
+// Add another object to your locations array. Set name to "easter egg", set "button text" to an array with the strings "2", "8", and "Go to town square?", set "button functions" to an array with the variables pickTwo, pickEight, and goTown, and text to "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!".
+
+// Solution: 
+
+{
+    name: "easter egg",
+        "button text": ["2", "8", "Go to town square?"],
+            "button functions": [pickTwo, pickEight, goTown],
+                text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
+}

4b22512a4db265125573c600d044aa437ef3576e
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-10 19:00:31 +0300

    commit-step-152-project-3

---
+++ step-152-project-3.js
@@ -0,0 +1,9 @@
+// Description: 
+
+// Now create the isMonsterHit function. This will return a boolean value (true or false) to be used in your if statement. Return the result of the comparison Math.random() > .2.
+
+// Solution: 
+
+function isMonsterHit() {
+    return Math.random() > .2
+}

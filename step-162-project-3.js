0d5c5255eca2bad84c56609e76b7b60c4e38e2d3
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-10 19:48:41 +0300

    commit-step-162-project-3

---
+++ step-162-project-3.js
@@ -0,0 +1,9 @@
+// Description: 
+
+// Inside pick, use const to initialize a variable named numbers and set it to an empty array.
+
+// Solution: 
+
+function pick(guess) {
+    const numbers = [];
+}

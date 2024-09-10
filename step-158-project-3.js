f7d94072beb14bdfb8d25309390036f297159b40
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-10 19:36:50 +0300

    commit-step-158-project-3

---
+++ step-158-project-3.js
@@ -0,0 +1,11 @@
+// Description: 
+
+// Now you can add a small easter egg (hidden feature) to your game.
+
+// Create a new function called easterEgg which calls the update function with locations[7] as the argument.
+
+// Solution: 
+
+function easterEgg() {
+    update(locations[7]);
+}

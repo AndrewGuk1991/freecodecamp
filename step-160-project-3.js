69e43d4f56afd463da6c7284176fd73c137cc559
AndrewGuk1991 <gukandrey91@gmail.com>
2024-09-10 19:42:49 +0300

    commit-step-160-project-3

---
+++ step-160-project-3.js
@@ -0,0 +1,14 @@
+// Description: 
+
+// Create two new functions named pickTwo and pickEight.
+
+// Inside each of those, call the pick() function and pass either 2 or 8 as the argument depending on the function name.
+
+// Solution: 
+
+function pickEight() {
+    pick(8)
+}
+function pickTwo() {
+    pick(2)
+}
